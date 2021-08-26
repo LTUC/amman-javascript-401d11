6f5874ca9dff1bf3e8dc770016c2bdefffec2466
Rawan <rawan.nujoom@gmail.com>
2021-08-26 12:07:37 +0300

    binary tree

---
+++ class-15/binary-tree/binary-tree.js
@@ -0,0 +1,45 @@
+'use strict';
+
+class BinaryTree {
+    constructor(root=null) {
+        this.root = root;
+    }
+
+    // root - left - right
+    preOrder() {
+        let result = [];
+        let traverse = (node) => {
+            result.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return result;
+    }
+
+    // left - root - right
+    inOrder() {
+        let result = [];
+        let traverse = (node) => {
+            if (node.left) traverse(node.left);
+            result.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+        traverse(this.root);
+        return result;
+    }
+
+    // left  - right - root
+    postOrder() {
+        let result = [];
+        let traverse = (node) => {
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            result.push(node.value);
+        }
+        traverse(this.root);
+        return result;
+    }
+}
+
+module.exports = BinaryTree;
