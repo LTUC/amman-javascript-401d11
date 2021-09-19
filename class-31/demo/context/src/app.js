5db85a08a383624eaecf484063d1d5aa21bf1dfd
Rawan <rawan.nujoom@gmail.com>
2021-09-19 11:50:17 +0300

    import node_modules in index.html

--- class-31/demo/context/src/app.js
+++ class-31/demo/context/src/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ThemeProvider from './context/theme';
 import SettingsProvider from './context/site';
-import 'normalize.css';
-import '@blueprintjs/core/lib/css/blueprint.css'
-import '@blueprintjs/icons/lib/css/blueprint-icons.css'
+// import 'normalize.css';
+// import '@blueprintjs/core/lib/css/blueprint.css'
+// import '@blueprintjs/icons/lib/css/blueprint-icons.css'
 
 import './app.css';
 import Main from './components/main';
